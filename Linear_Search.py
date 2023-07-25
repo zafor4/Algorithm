ca732fc0c73aada4ed980760893386edaa9ffe68
Zafor Iqbal <134352149+zafor4@users.noreply.github.com>
2023-07-25 08:10:10 +0600

    Update Linear_Search.py

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,8 +1,8 @@
 def linear_search(a,n):
     for i in range (0,len(a)):
         if (a[i]==n):
-            return i
-    return -1
+            return True
+    return False
 a=[1,2,3,4,5,6]
 n=4
 if (linear_search(a,n)==-1):
