b33fefe3a21a37e3face436daf42443ef7fc4f55
Zafor Iqbal <xoy4444@gmail.com>
2023-08-04 20:29:35 +0600

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,18 +1,18 @@
-def binary_search(a,n):
+def binary_search(numbers,number_to_find):
     low=0
-    high=len(a)-1
+    high=len(numbers)-1
     while(low<=high):
         mid=low+(high-low)//2
-        if(a[mid]==n):
+        if(numbers[mid]==number_to_find):
             return mid
-        elif (a[mid]<n):
+        elif (numbers[mid]<number_to_find):
             low=mid+1
         else :
             high=mid-1
     return -1
 a=list(map(int, input("elements of array:-").strip().split()))
-nf=int(input())
-if (binary_search(a,nf)==-1):
+n=int(input())
+if (binary_search(a,n)==-1):
     print("Not found")
 else :
-    print("Found in index",str(binary_search(a,nf)))
+    print("Found in index",str(binary_search(a,n)))
