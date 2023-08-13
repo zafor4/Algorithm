8410ab13ccc0a1fda35e3ed78312191836c17312
Zafor Iqbal <xoy4444@gmail.com>
2023-08-14 02:37:11 +0600

    sort

--- quick_Sort.py
+++ quick_Sort.py
@@ -5,3 +5,16 @@ def pertition(a,start,end):
         if(a[j]<=pivot):
             i=i+1
             a[i],a[j]=a[j],a[i]
+    a[i+1],a[end]=a[end],a[i+1]
+    return i+1
+def quick(a,start,end):
+    p=pertition(a,start,end)
+    if (start < end):
+        quick(a,start,p-1)
+        quick(a,p+1,end)
+
+a=[10,80,30,90,20,70,95,40]
+start=0
+end=len(a)-1
+quick(a,start,end)
+print(a)
